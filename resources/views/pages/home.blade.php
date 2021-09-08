baade4caa22025296d95fa794f223e56db9cdf94
MouadBNL <mouadbnl.2k01@gmail.com>
2021-09-08 15:13:14 +0100

    presentation section

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,10 +4,10 @@
     </x-slot>
    {{-- Hero section --}}
    <section id="hero" class="py-36">
-        <div class="container mx-auto">
+        <div class="container mx-auto p-4">
             <div class="text-center text-white mb-10">
                 <h1 class="title1">Construire un monde meilleur, un étudiant à la fois</h1>
-                <p class="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisi diam pharetra enim vivamus id consequat nibh. </p>
+                <p class="text-xl text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisi diam pharetra enim vivamus id consequat nibh. </p>
             </div>
             <div class="flex justify-center">
                 <a href="#" class="btn-primary">
@@ -15,6 +15,33 @@
                 </a>
             </div>
         </div>
+   </section>
+
+   {{-- presentation --}}
+   <section class="py-16">
+       <div class="container mx-auto lg:flex gap-4 p-4">
+            <div class="lg:w-1/3">
+                <img src="https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-photo-183042379.jpg" alt="Directeur">
+            </div>
+
+            <div class="w-2/3 flex items-center">
+                <div class="">
+                    <h2 class="title2 text-primary-500 mb-4">Porta nec, lacinia tellus ultricies</h2>
+                    <div class="mb-4">
+                        <p>
+                            “Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus consequat urna faucibus imperdiet et aliquam viverra. Porta nec, lacinia tellus ultricies quis interdum.
+                        </p> 
+                        <p>
+                            Egestas augue tristique amet, nullam nisl, tellus. Faucibus suspendisse lorem elit proin nulla tincidunt neque maecenas at. Quis ultrices ultrices euismod egestas morbi dictumst facilisis proin viverra”
+                        </p>
+                    </div>
+                    <div class="text-base text-gray-500">
+                        <span class="block font-extrabold">Wade Wilson</span>
+                        <span>Directeur</span>
+                    </div>
+                </div>
+            </div>
+       </div>
    </section>
 
 </x-layouts.base>

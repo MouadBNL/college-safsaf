54999ba2e36750f77df796f0d4e23880d77aea83
MouadBNL <mouadbnl.2k01@gmail.com>
2021-09-09 15:11:24 +0100

    adding maps

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -150,4 +150,35 @@
         </div>
    </section>
 
+   {{-- Map --}}
+   <section>
+    <div class="mapouter">
+        <div class="gmap_canvas">
+            <iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no"
+                marginheight="0" marginwidth="0"
+                src="https://maps.google.com/maps?q=Kenitra,%20college%20safsaf&t=&z=13&ie=UTF8&iwloc=&output=embed">
+            </iframe>
+        </div>
+        <style>
+            .mapouter {
+                position: relative;
+                text-align: right;
+                width: 100%;
+                height: 600px;
+            }
+
+            .gmap_canvas {
+                overflow: hidden;
+                background: none !important;
+                width: 100%;
+                height: 100%;
+            }
+
+            .gmap_iframe {
+                height: 100% !important;
+            }
+
+        </style>
+    </div>
+   </section>
 </x-layouts.base>
